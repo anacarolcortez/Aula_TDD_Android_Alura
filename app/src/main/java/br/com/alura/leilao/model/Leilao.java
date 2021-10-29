ec629a859e4ae56e6f4281c411d77c6e80e47656
acorteal <ana.cortez.alves@everis.com>
2021-10-29 13:27:24 -0300

    refatoração código e testes unitários

--- app/src/main/java/br/com/alura/leilao/model/Leilao.java
+++ app/src/main/java/br/com/alura/leilao/model/Leilao.java
@@ -22,11 +22,19 @@ public class Leilao implements Serializable {
 
     public void fazerLance(Lance lance){
         double valorLance = lance.getValorLance();
-        if (valorLance > maiorLance){
-            maiorLance = valorLance;
-        }
+        calculaMaiorLance(valorLance);
+        calculaMenorLance(valorLance);
+    }
+
+    private void calculaMenorLance(double valorLance) {
         if (valorLance < menorLance ){
                 menorLance = valorLance;
+        }
+    }
+
+    private void calculaMaiorLance(double valorLance) {
+        if (valorLance > maiorLance){
+            maiorLance = valorLance;
         }
     }
 
--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -5,72 +5,70 @@ import static org.junit.Assert.*;
 
 public class LeilaoTest {
 
+    private Leilao leilaoTeste = new Leilao("Leilão de Teste");
+    private Usuario ana = new Usuario("Ana");
+    private Usuario carol = new Usuario("Carol");
+
+
     @Test
     public void getDescricaoQuandoRecebeDescricao() {
-        Leilao leilaoTeste = new Leilao("Leilão de Teste");
         String descricao = leilaoTeste.getDescricao();
         assertEquals("Leilão de Teste", descricao);
     }
 
     @Test
     public void getDescricaoQuandoRecebeDescricaoNula() {
-        Leilao leilaoTeste = new Leilao(null);
-        String descricao = leilaoTeste.getDescricao();
+        Leilao leilaoDescNula = new Leilao(null);
+        String descricao = leilaoDescNula.getDescricao();
         assertEquals(null, descricao);
     }
 
     @Test
     public void getMaiorLanceQuandoHaUmLanceUnico() {
-        Leilao leilaoTeste = new Leilao("Leilão de Teste");
-        leilaoTeste.fazerLance(new Lance(new Usuario("Mariana"), 200.0));
+        leilaoTeste.fazerLance(new Lance(ana, 200.0));
         double maiorLanceDevolvido = leilaoTeste.getMaiorLance();
         assertEquals(200.0, maiorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void getMaiorLanceEmListaDeLeilaoCriadaEmOrdemCrescente() {
-        Leilao leilaoTeste = new Leilao("Leilão de Teste");
-        leilaoTeste.fazerLance(new Lance(new Usuario("Mariana"), 200.0));
-        leilaoTeste.fazerLance(new Lance(new Usuario("Mari"), 300.0));
-        leilaoTeste.fazerLance(new Lance(new Usuario("Mar"), 400.0));
-        leilaoTeste.fazerLance(new Lance(new Usuario("Ma"), 500.0));
+        leilaoTeste.fazerLance(new Lance(ana, 200.0));
+        leilaoTeste.fazerLance(new Lance(carol, 300.0));
+        leilaoTeste.fazerLance(new Lance(ana, 400.0));
+        leilaoTeste.fazerLance(new Lance(carol, 500.0));
         double maiorLanceDevolvido = leilaoTeste.getMaiorLance();
         assertEquals(500.0, maiorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void getMaiorLanceEmListaDeLeilaoCriadaEmOrdemDecrescente() {
-        Leilao leilaoTeste = new Leilao("Leilão de Teste");
-        leilaoTeste.fazerLance(new Lance(new Usuario("Ma"), 500.0));
-        leilaoTeste.fazerLance(new Lance(new Usuario("Mar"), 400.0));
-        leilaoTeste.fazerLance(new Lance(new Usuario("Mari"), 300.0));
-        leilaoTeste.fazerLance(new Lance(new Usuario("Mariana"), 200.0));
+        leilaoTeste.fazerLance(new Lance(ana, 500.0));
+        leilaoTeste.fazerLance(new Lance(carol, 400.0));
+        leilaoTeste.fazerLance(new Lance(ana, 300.0));
+        leilaoTeste.fazerLance(new Lance(carol, 200.0));
         double maiorLanceDevolvido = leilaoTeste.getMaiorLance();
         assertEquals(500.0, maiorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void getMenorLanceQuandoHaUmLanceUnico() {
-        Leilao leilaoTeste = new Leilao("Leilão de Teste");
-        leilaoTeste.fazerLance(new Lance(new Usuario("Mariana"), 50.0));
+        leilaoTeste.fazerLance(new Lance(ana, 50.0));
         double menorLanceDevolvido = leilaoTeste.getMenorLance();
         assertEquals(50.0, menorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void getMenorLanceEmListaDeLeilaoCriadaEmOrdemCrescente() {
-        Leilao leilaoTeste = new Leilao("Leilão de Teste");
-        leilaoTeste.fazerLance(new Lance(new Usuario("Mariana"), 200.0));
-        leilaoTeste.fazerLance(new Lance(new Usuario("Mari"), 300.0));
-        leilaoTeste.fazerLance(new Lance(new Usuario("Mar"), 400.0));
-        leilaoTeste.fazerLance(new Lance(new Usuario("Ma"), 500.0));
+        leilaoTeste.fazerLance(new Lance(ana, 500.0));
+        leilaoTeste.fazerLance(new Lance(carol, 400.0));
+        leilaoTeste.fazerLance(new Lance(ana, 300.0));
+        leilaoTeste.fazerLance(new Lance(carol, 200.0));
         double menorLanceDevolvido = leilaoTeste.getMenorLance();
         assertEquals(200.0, menorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void getMenorLanceEmListaDeLeilaoCriadaEmOrdemDecrescente() {
-        Leilao leilaoTeste = new Leilao("Leilão de Teste");
         leilaoTeste.fazerLance(new Lance(new Usuario("Ma"), 500.0));
         leilaoTeste.fazerLance(new Lance(new Usuario("Mar"), 400.0));
         leilaoTeste.fazerLance(new Lance(new Usuario("Mari"), 300.0));
